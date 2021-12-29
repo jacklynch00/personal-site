78efb1ce88208c940519ecb208ded02b33ec0e18
Jack Lynch <jrlynch00@gmail.com>
2021-12-29 10:58:55 -0500

    Updated my data for the about me page

--- constants/data.js
+++ constants/data.js
@@ -13,10 +13,15 @@ const userData = {
 		},
 	],
 	about: {
-		title: '',
-		description: [``, ``, ``],
-		currentProject: 'TBD...',
-		currentProjectUrl: '',
+		title: 'I am Jack Lynch',
+		description: [
+			`It has been quite the ride to where I am today. In the beginning of high school in 2014, I learned how to create a big red square that when clicked, would send you to Google. CRAZY, I know!! From then on, I've been constantly tinkering and building smaller projects.`,
+			`After high school I decided to pursue a degree in Computer Science and Engineering at Ohio State in 2018, but was very quickly met with many challenges and struggled in the early parts of my college career.`,
+			`However, after some readjustment and a LOT of hard work and summer classes, I was able to get back on track and will graduate in Spring of 2022 with a degree in Information Systems and a minor in Entrepreneurship & Innovation. I was also fortunate enough to work at a few different internships throughout college. Check out some of my experiences on my experience page!`,
+			`I am currently working at my most recent internship with Ubihere building web dashboards using React, Django, and other technologies. I spend a lot of my time now learning by completing online courses, reading articles/documentation and completing tutorials.`,
+		],
+		currentProject: 'This Website!',
+		currentProjectUrl: 'https://www.jacklynch.dev',
 	},
 	experience: [
 		{
@@ -27,7 +32,7 @@ const userData = {
 			desc: 'Description goes here.',
 		},
 	],
-	resumeUrl: 'add+google+drive+resume+link+here',
+	resumeUrl: 'https://drive.google.com/file/d/1EZj4JzbPd7FlvTgOK3hIjq8HVFdunEl1/view?usp=sharing',
 	socialLinks: {
 		instagram: 'https://www.instagram.com/jack_lynch00',
 		twitter: 'https://twitter.com/jack_lynch00',
