6d4fe4cb0a2db245d0228aaf9fecddc3a0bdb08f
Jack Lynch <jrlynch00@gmail.com>
2022-02-03 08:36:36 -0500

    Connected contact form

---
+++ components/ContainerModule.ts
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const ContainerModule: NextPage = ({ children, ...customeMeta }) => {};
+
+export default ContainerModule;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	env: {
-		EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
-		EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID,
-		EMAIL_USER_ID: process.env.EMAIL_USER_ID,
-		EMAIL_ACCESS_TOKEN: process.env.EMAIL_ACCESS_TOKEN,
-	},
-};
+  reactStrictMode: true,
+}
