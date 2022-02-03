81d437c157d2fb18b4d8cb0c510b836551bb515a
Jack Lynch <jrlynch00@gmail.com>
2022-02-03 08:45:28 -0500

    Removing un-needed ts files

--- components/ContainerModule.ts
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import React from 'react';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-
-const ContainerModule: NextPage = ({ children, ...customeMeta }) => {};
-
-export default ContainerModule;
